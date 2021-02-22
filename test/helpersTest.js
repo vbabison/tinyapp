a760e1404d4840e25c787ce541da181c1a7ed250
Vbabison <officialbookingssf@gmail.comm>
2021-02-22 03:18:38 -0500

    lint check

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,25 +4,27 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedOutput = "userRandomID";
+    assert(user, expectedOutput);
   });
   it('should return undefined if user not found', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedOutput = "userRandomID";
+    assert(user, expectedOutput);
   });
 });
 
